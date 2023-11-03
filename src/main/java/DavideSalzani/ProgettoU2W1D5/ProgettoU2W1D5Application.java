a5fba9d23e2b03c83a1ba43d07e2f781a28e9122
Salza99 <137687037+Salza99@users.noreply.github.com>
2023-11-03 14:03:24 +0100

    initialize springboot project

---
+++ src/main/java/DavideSalzani/ProgettoU2W1D5/ProgettoU2W1D5Application.java
@@ -0,0 +1,13 @@
+package DavideSalzani.ProgettoU2W1D5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoU2W1D5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoU2W1D5Application.class, args);
+	}
+
+}
---
+++ src/test/java/DavideSalzani/ProgettoU2W1D5/ProgettoU2W1D5ApplicationTests.java
@@ -0,0 +1,13 @@
+package DavideSalzani.ProgettoU2W1D5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoU2W1D5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
